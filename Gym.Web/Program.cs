17386cea007a5b8c4a945ded5cb4347cc1d83617
Toan <phanqu6ctoan@gmail.com>
2023-11-14 04:53:10 +0700

    Account verification,Change password,Forgot password

--- Gym.Web/Program.cs
+++ Gym.Web/Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.
 	GetConnectionString("DefaultConnection")));
 
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
 builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
 {
     options.SignIn.RequireConfirmedAccount = true;
